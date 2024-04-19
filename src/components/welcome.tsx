bca3c82231a47d5ddd666c1eb2848f320e4eb262
chvndler <chvndler.ch@icloud.com>
2024-04-19 01:14:55 -0400

    ups

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -3,9 +3,7 @@ export const Welcome = () => {
     <main className='mx-auto max-w-7xl px-6 lg:px-16'>
       <section className='mb-16 mt-20 flex flex-col items-start justify-between gap-x-12 lg:mt-32 xl:flex-row xl:items-center xl:justify-center'>
         <div className='flex flex-col items-center'>
-          <h1 className='text-center text-6xl font-bold'>
-            Welcome to the Sanity Next.js Portfolio Site
-          </h1>
+          <h1 className='text-center text-6xl font-bold'>irradiance studio</h1>
           <p className='mt-6 text-center text-xl'>Welcome.</p>
         </div>
       </section>
