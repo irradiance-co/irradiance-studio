3215a1bffa006c3360ee67e09e0658cd6517f5db
chvndler <chvndler.ch@icloud.com>
2024-04-24 11:28:39 -0400

    add playground

---
+++ src/app/(play)/play/page.tsx
@@ -0,0 +1,7 @@
+export default async function Play() {
+  return (
+    <section className='flex h-screen items-center justify-center'>
+      <div className='z-50 flex flex-col items-center space-y-4'>PLAY</div>
+    </section>
+  );
+}
