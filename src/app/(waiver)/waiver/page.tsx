39f9791a5c65520bbb73eda8d3e5d4f316b820df
chvndler <chvndler.ch@icloud.com>
2024-07-03 02:09:59 -0400

    Update page.tsx

--- src/app/(waiver)/waiver/page.tsx
+++ src/app/(waiver)/waiver/page.tsx
@@ -1,12 +1,12 @@
-import { GradientLogo } from '@/components/gradient-logo';
-
 export default function Waiver() {
   return (
     <section className='flex h-auto items-center justify-center'>
-      <div className='z-50 flex flex-col items-center'>
+      <div className='z-100 flex flex-col items-center'>
         <div className='relative mx-auto w-full items-center px-2'>
           <div className='mx-auto mt-24 flex w-full flex-col pt-6'>
-            <div className='klaviyo-form-SRkHVB'></div>
+            {/** <div className='klaviyo-form-SRkHVB'></div> **/}
+
+            <div className='klaviyo-form-WCqKBA'></div>
           </div>
         </div>
       </div>
