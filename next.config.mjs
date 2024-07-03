3860686eff2f3925946f4f5486a38b57c856cebf
chvndler <chvndler.ch@icloud.com>
2024-07-03 01:33:32 -0400

    added waiver

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.klaviyo.com *.sanity.io klaviyo.com a.klaviyo.com vercel.com va.vercel-scripts.com *.vercel-scripts.com;
-    style-src 'self' 'unsafe-inline';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.klaviyo.com *.sanity.io klaviyo.com a.klaviyo.com *.a.klaviyo.com vercel.com va.vercel-scripts.com *.vercel-scripts.com fast.a.klaviyo.com static-forms.klaviyo.com;
+    connect-src 'self' *.klaviyo.com *.sanity.io fast.a.klaviyo.com static-forms.klaviyo.com a.klaviyo.com *.a.klaviyo.com api-js.datadome.co;
+    style-src 'self' 'unsafe-inline' static.klaviyo.com *.klaviyo.com;
     img-src 'self' blob: data: *.klaviyo.com *.sanity.io;
-    font-src 'self';
+    font-src 'self' *.klaviyo.com;
 `;
 
 /** @type {import('next').NextConfig} */
